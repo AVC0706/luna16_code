195665d858d7e515355c1126d57192b0b46f2501
AVC0706 <atharvvijay4@gmail.com>
2020-11-24 21:32:27 +0530

    Basic Dset.py

--- dset.py
+++ dset.py
@@ -1,1 +1,20 @@
-#testing
+import os
+import glob
+
+import functools
+from collections import namedtuple
+
+CandidateInfoTuple = namedtuple(
+    'CandidateInfoTuple',
+    'isNodule_bool, diameter_mm, series_uid, center_xyz',
+)
+
+@functools.lru_cache(1)
+def getCandidateInfoList(requireOnDisk_bool=True):
+    # We construct a set with all series_uids that are present on disk.
+    # This will let us use the data, even if we haven't downloaded all of
+    # the subsets yet
+    mhd_list = glob.glob('/kaggle/input/luna16/subset0/subset0/*.mhd')
+    presentOnDisk_set = {os.path.split(p)[-1][:-4] for p in mhd_list}   #Takes path as input and returns the series_uuid from path
+
+

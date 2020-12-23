f22d7a8db660c36be593dfd8fdb23f07677bb295
AVC0706 <atharvvijay4@gmail.com>
2020-12-23 19:17:16 +0530

    Training Code

--- dset.py
+++ dset.py
@@ -3,44 +3,56 @@ import csv
 import functools
 import glob
 import os
+import random
+
+from collections import namedtuple
+
 import SimpleITK as sitk
 import numpy as np
-from collections import namedtuple
+
+import torch
+import torch.cuda
+from torch.utils.data import Dataset
+
+# from util.disk import getCache
 from util.util import XyzTuple, xyz2irc
+from util.logconf import logging
+
+log = logging.getLogger(__name__)
+# log.setLevel(logging.WARN)
+# log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
+
+# raw_cache = getCache('part2ch11_raw')
 
 CandidateInfoTuple = namedtuple(
     'CandidateInfoTuple',
     'isNodule_bool, diameter_mm, series_uid, center_xyz',
 )
 
+dsetLocation = "../dataset"
 
-@functools.lru_cache(1)
+# @functools.lru_cache(1)
 def getCandidateInfoList(requireOnDisk_bool=True):
     # We construct a set with all series_uids that are present on disk.
     # This will let us use the data, even if we haven't downloaded all of
-    # the subsets yet
-    mhd_list = glob.glob('/kaggle/input/luna16/subset0/subset0/*.mhd')
-    # Takes path as input and returns the series_uuid from path
+    # the subsets yet.
+    mhd_list = glob.glob(dsetLocation+'/subset0/*.mhd')
     presentOnDisk_set = {os.path.split(p)[-1][:-4] for p in mhd_list}
 
-    print(presentOnDisk_set)
-
     diameter_dict = {}
-
-    with open('/kaggle/input/luna16/annotations.csv', 'r') as f:
+    with open(dsetLocation+'/annotations.csv', "r") as f:
         for row in list(csv.reader(f))[1:]:
             series_uid = row[0]
             annotationCenter_xyz = tuple([float(x) for x in row[1:4]])
             annotationDiameter_mm = float(row[4])
 
             diameter_dict.setdefault(series_uid, []).append(
-                (annotationCenter_xyz, annotationDiameter_mm)
+                (annotationCenter_xyz, annotationDiameter_mm),
             )
 
     candidateInfo_list = []
-
-
-    with open('/kaggle/input/luna16/candidates.csv', 'r') as f:
+    with open(dsetLocation+'/candidates.csv', "r") as f:
         for row in list(csv.reader(f))[1:]:
             series_uid = row[0]
 
@@ -51,39 +63,31 @@ def getCandidateInfoList(requireOnDisk_bool=True):
             candidateCenter_xyz = tuple([float(x) for x in row[1:4]])
 
             candidateDiameter_mm = 0.0
-            for annotation_tuple in diameter_dict.get(series_uid, []):
-                annotationCenter_xyz, annotationDiameter_mm = annotation_tuple
-
+            for annotation_tup in diameter_dict.get(series_uid, []):
+                annotationCenter_xyz, annotationDiameter_mm = annotation_tup
                 for i in range(3):
-                    delta_mm = abs(
-                        annotationCenter_xyz[i] - candidateCenter_xyz[i])
-
+                    delta_mm = abs(candidateCenter_xyz[i] - annotationCenter_xyz[i])
                     if delta_mm > annotationDiameter_mm / 4:
                         break
-                    else:
-                        candidateDiameter_mm = annotationDiameter_mm
-                        break
+                else:
+                    candidateDiameter_mm = annotationDiameter_mm
+                    break
 
             candidateInfo_list.append(CandidateInfoTuple(
                 isNodule_bool,
                 candidateDiameter_mm,
                 series_uid,
-                candidateCenter_xyz
+                candidateCenter_xyz,
             ))
 
-        candidateInfo_list.sort(reverse=True)
-
-        # print(candidateInfo_list[-1][2])
-
-        return candidateInfo_list
-
-# getCandidateInfoList()
-
+    candidateInfo_list.sort(reverse=True)
+    return candidateInfo_list
 
 class Ct:
     def __init__(self, series_uid):
         mhd_path = glob.glob(
-            '/kaggle/input/luna16/subset0/subset0/{}.mhd'.format(series_uid))[0]
+            dsetLocation+'/subset0/{}.mhd'.format(series_uid)
+        )[0]
 
         ct_mhd = sitk.ReadImage(mhd_path)
         ct_a = np.array(sitk.GetArrayFromImage(ct_mhd), dtype=np.float32)
@@ -135,21 +139,23 @@ class Ct:
         return ct_chunk, center_irc
 
 
-@functools.lru_cache(1, typed=True)
+# @functools.lru_cache(1, typed=True)
 def getCt(series_uid):
     return Ct(series_uid)
 
-@raw_cache.memoize(typed=True)
+# @raw_cache.memoize(typed=True)
 def getCtRawCandidate(series_uid, center_xyz, width_irc):
     ct = getCt(series_uid)
     ct_chunk, center_irc = ct.getRawCandidate(center_xyz, width_irc)
     return ct_chunk, center_irc
+
 
 class LunaDataset(Dataset):
     def __init__(self,
                  val_stride=0,
                  isValSet_bool=None,
                  series_uid=None,
+                 sortby_str='random',
             ):
         self.candidateInfo_list = copy.copy(getCandidateInfoList())
 
@@ -166,16 +172,27 @@ class LunaDataset(Dataset):
             del self.candidateInfo_list[::val_stride]
             assert self.candidateInfo_list
 
-        # log.info("{!r}: {} {} samples".format(
-        #     self,
-        #     len(self.candidateInfo_list),
-        #     "validation" if isValSet_bool else "training",
-        # ))
+
+        if sortby_str == 'random':
+            random.shuffle(self.candidateInfo_list)
+        elif sortby_str == 'series_uid':
+            self.candidateInfo_list.sort(key=lambda x: (x.series_uid, x.center_xyz))
+        elif sortby_str == 'label_and_size':
+            pass
+        else:
+            raise Exception("Unknown sort: " + repr(sortby_str))
+
+        log.info("{!r}: {} {} samples".format(
+            self,
+            len(self.candidateInfo_list),
+            "validation" if isValSet_bool else "training",
+        ))
 
     def __len__(self):
         return len(self.candidateInfo_list)
 
     def __getitem__(self, ndx):
+        print('---------get item --------')
         candidateInfo_tup = self.candidateInfo_list[ndx]
         width_irc = (32, 48, 48)
 
@@ -184,9 +201,7 @@ class LunaDataset(Dataset):
             candidateInfo_tup.center_xyz,
             width_irc,
         )
-
-        candidate_t = torch.from_numpy(candidate_a)
-        candidate_t = candidate_t.to(torch.float32)
+        candidate_t = torch.from_numpy(candidate_a).to(torch.float32)
         candidate_t = candidate_t.unsqueeze(0)
 
         pos_t = torch.tensor([
@@ -196,9 +211,4 @@ class LunaDataset(Dataset):
             dtype=torch.long,
         )
 
-        return (
-            candidate_t,
-            pos_t,
-            candidateInfo_tup.series_uid,
-            torch.tensor(center_irc),
-        )
+        return candidate_t, pos_t, candidateInfo_tup.series_uid, torch.tensor(center_irc)
